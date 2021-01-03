35f47767861a1733e69b544ab01e0d6533002b90
Ubuntu <ubuntu@ip-172-31-1-65.us-west-1.compute.internal>
2021-01-03 01:31:49 +0000

    portfolio init

---
+++ oluwafemi-portfolio/routes/main.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const router = express.Router()
+
+router.get('/', (req,res) => {
+
+
+    res.send('Welcome to my Portfolio')
+
+    
+})
+
+
+
+
+module.exports()
